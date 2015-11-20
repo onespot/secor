e796572026432ad9e7955e1f9d284571215ef0b9
Chris Reeves <reevesc@gmail.com>
2015-11-20 11:22:58 -0600

    Remove bad test

--- src/test/java/com/pinterest/secor/io/impl/AvroFileReaderWriterFactoryTest.java
+++ src/test/java/com/pinterest/secor/io/impl/AvroFileReaderWriterFactoryTest.java
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,19 +21,7 @@ import com.pinterest.secor.common.LogFilePath;
 import com.pinterest.secor.io.FileReader;
 import com.pinterest.secor.io.FileWriter;
 import com.pinterest.secor.io.KeyValue;
-import org.apache.avro.Schema;
-import org.apache.avro.file.DataFileReader;
-import org.apache.avro.generic.GenericDatumReader;
-import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.DatumReader;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import java.io.File;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class AvroFileReaderWriterFactoryTest {
     private AvroFileReaderWriterFactory mFactory;
@@ -76,12 +64,12 @@ public class AvroFileReaderWriterFactoryTest {
 
         KeyValue kvout = fileReader.next();
         System.out.println(new String(kvout.getValue()));
-     //   assertEquals(kv1.getKey(), kvout.getKey());
-     //   assertArrayEquals(kv1.getValue(), kvout.getValue());
+        //   assertEquals(kv1.getKey(), kvout.getKey());
+        //   assertArrayEquals(kv1.getValue(), kvout.getValue());
         kvout = fileReader.next();
         System.out.println(new String(kvout.getValue()));
-     //   assertEquals(kv2.getKey(), kvout.getKey());
-     //   assertArrayEquals(kv2.getValue(), kvout.getValue());
+        //   assertEquals(kv2.getKey(), kvout.getKey());
+        //   assertArrayEquals(kv2.getValue(), kvout.getValue());
     }
 
     @Test
